3a0cf8915ab9d11a83f1e652c7f83af45edde644
darma <damabigma@gmail.com>
2022-11-09 23:58:15 +0800

    init: 仓库init

---
+++ src/components/testMa.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>testma</div>
+</template>
+
+<script setup lang="ts">
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { watch } from 'vue'
+
+const { result, loading } = useQuery(gql`
+  query getUsers {
+    hero {
+      name
+      id
+    }
+  }
+`)
+
+watch(loading, (val) => {
+  if (val === false) {
+    console.log(result.value)
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
